f58eecd19b98ada1e23fcfac741109a01e0db62d
prakash <prakash.engg89@gmail.com>
2017-07-15 16:21:40 +0530

    Changes committed

--- public/js/controllers/login-ctrl.js
+++ public/js/controllers/login-ctrl.js
@@ -3,52 +3,54 @@
  */
 
 angular.module('RDash')
-    .controller('LoginCtrl', ['$scope', '$location', 'AuthServices', '$http', 'toaster', 'Utils', LoginCtrl]);
+  .controller('LoginCtrl', ['$scope', '$location', 'AuthServices', '$http', 'toaster', 'Utils', '$cookieStore', LoginCtrl]);
 
-function LoginCtrl($scope, $location, AuthServices, $http, toaster, Utils) {
-    $scope.users = [];
+function LoginCtrl($scope, $location, AuthServices, $http, toaster, Utils, $cookieStore) {
+  $scope.users = [];
 
-    $scope.init = function() {
-        $scope.user = {
-            name: '',
-            password: ''
-        };
+  $scope.init = function() {
+    $scope.user = {
+      name: '',
+      password: ''
     };
+  };
 
-    $scope.loginUser = function(userData) {
-        if( userData.name == '' || userData.password == '' ) {
+  $scope.loginUser = function(userData) {
+    if (userData.name == '' || userData.password == '') {
+      toaster.pop({
+        type: 'error',
+        body: 'Please fill out all fields!...',
+        timeout: 1000
+      });
+    } else {
+      var userInfo = {
+        username: userData.name,
+        password: Utils.generateHashPassword(userData.password)
+      };
+      AuthServices.authenticateUser(userInfo)
+        .success(function(data) {
+          if (data.code == 200) {
+            // Put cookie
+            $cookieStore.put('userName', data.userdata.name);
+            if (data.userdata.role_name == 'admin') {
+              $location.path('/admin/dashboard');
+            } else if (data.userdata.role_name == 'user') {
+              $location.path('/users/dashboard');
+            }
+          } else {
             toaster.pop({
-                type: 'error',
-                body: 'Please fill out all fields!...',
-                timeout: 1000
+              type: 'error',
+              body: data.message,
+              timeout: 1000
             });
-        } else {
-            var userInfo = {
-                username: userData.name,
-                password: Utils.generateHashPassword(userData.password)
-            };
-            AuthServices.authenticateUser(userInfo)
-                .success(function(data) {
-                    if ( data.code == 200 ) {
-                      if( data.userdata.role_name == 'admin' ) {
-                          $location.path('/admin/dashboard');
-                        } else if( data.userdata.role_name == 'user' ) {
-                            $location.path('/users/dashboard');
-                        }
-                    } else {
-                        toaster.pop({
-                            type: 'error',
-                            body: data.message,
-                            timeout: 1000
-                        });
-                    }
-                }).error(function(error) {
-                    console.log(error);
-                });
-        }
+          }
+        }).error(function(error) {
+          console.log(error);
+        });
+    }
 
-    };
+  };
 
-    //Call function initialization
-    $scope.init();
+  //Call function initialization
+  $scope.init();
 }
